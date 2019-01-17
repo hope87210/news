de4c28e932492c7388b848154cfc14068478fbd6
hope87210 <39110439+hope87210@users.noreply.github.com>
2019-01-17 18:17:58 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,82 @@
+window.onload=function(){
+    var obt1=document.getElementById("bt1");
+    var odiv1=document.getElementById("text1");
+
+
+    obt1.onclick=function(){
+      if(odiv1.style.display=="none"){
+        odiv1.style.display="block";
+        obt1.value="conceal";
+        }
+      else{
+        odiv1.style.display="none";
+        obt1.value="more";
+        }
+    }
+
+    var obt2=document.getElementById("bt2");
+    var odiv2=document.getElementById("text2");
+
+
+    obt2.onclick=function(){
+      if(odiv2.style.display=="none"){
+        odiv2.style.display="block";
+        obt2.value="conceal";
+        }
+      else{
+        odiv2.style.display="none";
+        obt2.value="more";
+        }
+    }
+
+
+    var obt3=document.getElementById("bt3");
+    var odiv3=document.getElementById("text3");
+
+
+    obt3.onclick=function(){
+      if(odiv3.style.display=="none"){
+        odiv3.style.display="block";
+        obt3.value="conceal";
+        }
+      else{
+        odiv3.style.display="none";
+        obt3.value="more";
+        }
+    }
+
+
+
+    var obt4=document.getElementById("bt4");
+    var odiv4=document.getElementById("text4");
+
+
+    obt4.onclick=function(){
+      if(odiv4.style.display=="none"){
+        odiv4.style.display="block";
+        obt4.value="conceal";
+        }
+      else{
+        odiv4.style.display="none";
+        obt4.value="more";
+        }
+    }
+
+
+
+    var obt5=document.getElementById("bt5");
+    var odiv5=document.getElementById("text5");
+
+
+    obt5.onclick=function(){
+      if(odiv5.style.display=="none"){
+        odiv5.style.display="block";
+        obt5.value="conceal";
+        }
+      else{
+        odiv5.style.display="none";
+        obt5.value="more";
+        }
+    }
+
+}
